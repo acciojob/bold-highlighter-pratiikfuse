08d745e210d9d7e5b5ef1100a16a0134252ea7ec
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-29 15:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let boldElements = document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
-
+	for(let i = 0;i<boldElements.length;i++)
+		{
+			boldElements[i].style.color  = "green";
+		}
 }
 
 
 function return_normal() {
     //Write your code here
+		for(let i = 0;i<boldElements.length;i++)
+		{
+			boldElements[i].style.color  = "black";
+		}
 
     
 }
